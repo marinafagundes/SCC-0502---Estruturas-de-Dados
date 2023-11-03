1a9e91709c7bb8cdd48fbbacbc4aa6ed245c709b
Marina Fagundes <138703555+marinafagundes@users.noreply.github.com>
2023-11-02 21:45:10 -0300

    Add files via upload

---
+++ Pilha Estatica/PilhaEstatica.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "PilhaEstatica.h"
+
+
+void inicializar(t_pilha *P) {
+    P->topo = -1;
+}
+
+
+int push(int e, t_pilha *P) {
+
+    if (cheia(P)) {
+        printf("Pilha cheia\n");
+        return CHEIA;
+    }
+
+    P->topo++;
+    P->pilha[P->topo] = e;
+
+    return SUCESSO;
+
+}
+
+int pop(t_pilha *P) {
+
+    if (vazia(P)) {
+        printf("Nao pode popar\n");
+        return VAZIA;
+    }
+
+    int e = P->pilha[P->topo];
+    P->topo--;
+
+    return e;
+
+}
+
+int vazia(t_pilha *P) {
+    return P->topo < 0;
+}
+
+int cheia(t_pilha *P) {
+    // return (P->topo == MAX - 1) ? 1 : 0;
+    // return P->topo == MAX -1;
+    return P->topo == MAX - 1;
+}
+
+int topo(t_pilha *P) {
+
+    if (vazia(P)) {
+        printf("Nao tem topo!!!!\n");
+        return VAZIA;
+    }
+    return P->pilha[P->topo];
+
+}
+
+
+int contar(t_pilha *P) {
+    return P->topo + 1;
+}
